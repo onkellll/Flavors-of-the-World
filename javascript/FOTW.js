b5968ec5af8d601148a548a3b9dd51816ea17f9d
Iselin <iselin.steiner@gmail.com>
2024-10-15 18:04:26 +0200

    first commit

---
+++ javascript/FOTW.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+var prevScrollpos = window.pageYOffset; window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset; if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
