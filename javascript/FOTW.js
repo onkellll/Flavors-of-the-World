7d96d9ef223b259bd6215fe99b835e37567eaf67
onkellll <e-iseste2@iskule.no>
2024-10-21 09:07:04 +0200

    Add files via upload

--- javascript/FOTW.js
+++ javascript/FOTW.js
@@ -1,12 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
 var prevScrollpos = window.pageYOffset; window.onscroll = function() {
     var currentScrollPos = window.pageYOffset; if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
